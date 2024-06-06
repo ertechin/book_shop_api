2df90b80aec256dcc1ca996af26d665b6138ab7d
ertechin <erselertekin@hotmail.com>
2024-06-07 01:50:23 +0300

    add payment_price positive validation

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_total_price, only: :create
 
   def create
     if Payment.process(current_user)
@@ -7,5 +8,13 @@ class PaymentsController < ApplicationController
     else
       render json: { message: 'Payment failed' }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def check_total_price
+    return if current_user.basket.total_price.positive?
+
+    render json: { message: 'Basket is empty' }, status: :unprocessable_entity
   end
 end
